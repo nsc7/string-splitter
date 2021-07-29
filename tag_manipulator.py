cd5a70e71affe38d6968f2d1c820a325a1c5194a
Sam <samantha.chinomona@gmail.com>
2021-07-29 16:31:17 +0100

    cut3 added 5 tests

--- tag_manipulator.py
+++ tag_manipulator.py
@@ -1,8 +1,15 @@
+import re
+
+
 class TagManipulator():
-    def parse_string(self, tags):
+    def parse_string(self, tags, regex=""):
         result = []
 
-        if len(tags) < 1:
-            return result
+        temp_res = re.split(regex, tags)
+        if (len(temp_res[0]) > 0):
+            result = temp_res
 
-        return result
+        trimmed = []
+        for item in result:
+            trimmed.append(item.strip())
+        return trimmed
--- test_tag_manipulator.py
+++ test_tag_manipulator.py
@@ -42,3 +42,78 @@ def test_split_one_string_result_array_of_one():
 
     # assert
     assert result == expResult
+
+
+def test_split_one_string_result_array_of_one_no_whitespace():
+    # arrange
+    stringToSplit = "java"
+    regex = ","
+    expResult = ["java"]
+    result = None
+    cut = TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit, regex)
+
+    # assert
+    assert result == expResult
+
+
+def test_split_one_string_result_array_of_one_begin_whitespace():
+    # arrange
+    stringToSplit = " java"
+    regex = ","
+    expResult = ["java"]
+    result = None
+    cut = TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit, regex)
+
+    # assert
+    assert result == expResult
+
+
+def test_split_one_string_result_array_of_one_end_whitespace():
+    # arrange
+    stringToSplit = "java "
+    regex = ","
+    expResult = ["java"]
+    result = None
+    cut = TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit, regex)
+
+    # assert
+    assert result == expResult
+
+
+def test_split_one_string_result_array_of_one_both_whitespace():
+    # arrange
+    stringToSplit = " java "
+    regex = ","
+    expResult = ["java"]
+    result = None
+    cut = TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit, regex)
+
+    # assert
+    assert result == expResult
+
+
+def test_split_one_string_result_array_of_two_words_whitespace():
+    # arrange
+    stringToSplit = "java, python "
+    regex = ","
+    expResult = ["java", "python"]
+    result = None
+    cut = TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit, regex)
+
+    # assert
+    assert result == expResult
