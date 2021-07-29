e82709465c73376b9feb57b13008724e5febd8dd
Sam <samantha.chinomona@gmail.com>
2021-07-29 16:09:01 +0100

    cut2 test with comma

--- test_tag_manipulator.py
+++ test_tag_manipulator.py
@@ -13,3 +13,32 @@ def test_split_empty_string_result_empty_array():
 
     # assert
     assert result == expResult
+
+
+def test_split_comma_empty_string_result_empty_array():
+    # arrange
+    stringToSplit = ","
+    expResult = []
+    result = None
+    cut = TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit)
+
+    # assert
+    assert result == expResult
+
+
+def test_split_one_string_result_array_of_one():
+    # arrange
+    stringToSplit = "java"
+    regex = ","
+    expResult = ["java"]
+    result = None
+    cut = TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit, regex)
+
+    # assert
+    assert result == expResult
